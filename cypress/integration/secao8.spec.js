29acba1de27c85dcd68695a57a027629aca20dce
LeonardoPhildeg <leonardo.phildeg@bridge.ufsc.br>
2020-09-18 18:32:51 -0300

    secao 8

--- cypress/integration/secao8.spec.js
+++ cypress/integration/secao8.spec.js
@@ -15,4 +15,48 @@ describe('secao 8', () => {
         cy.xpath('//div[@id=\'resultado\']/span')
             .should('contain', 'Cadastrado!')
     })
+
+    it('Command clickAlert', () => {
+      cy.clickAlert('#alert', 'Alert Simples')
+    })
+
+    const foods = ['Carne', 'Frango', 'Pizza', 'Vegetariano']
+    foods.forEach((food) => {
+        it(`TESTE DINÂMICO - Cadastro com comida ${food}`, () => {
+            cy.get('#formNome').type('usuario.nome')
+            cy.get('#formSobrenome').type('usuario.sobrenome')
+            cy.get(`[name=formSexo][value=F]`).click()
+            cy.xpath(`//label[contains(., '${food}')]/preceding-sibling::input`).click()
+            cy.get('#formEscolaridade').select('Doutorado')
+            cy.get('#formEsportes').select('Corrida')
+            cy.get('#formCadastrar').click()
+            cy.xpath('//div[@id=\'resultado\']/span')
+                .should('contain', 'Cadastrado!')
+        })
+    })
+
+    it('Deve selecionar os itens usando each, menos o vegetariano', () => {
+        cy.get('#formNome').type('usuario.nome')
+        cy.get('#formSobrenome').type('usuario.sobrenome')
+        cy.get(`[name=formSexo][value=F]`).click()
+        cy.get('[name=formComidaFavorita]').each((element) => {
+            if(element.val() !== 'vegetariano'){
+                cy.wrap(element).click()
+            }
+        })
+        cy.get('#formEscolaridade').select('Doutorado')
+        cy.get('#formEsportes').select('Corrida')
+        cy.get('#formCadastrar').click()
+        cy.xpath('//div[@id=\'resultado\']/span')
+            .should('contain', 'Cadastrado!')
+    })
+
+    it('Comando CLOCK', () => {
+        cy.clock(new Date(2020, 0, 13))
+        cy.get('#buttonNow').click()
+        cy.get('#resultado > span').should('contain', '13/01/2020')
+    })
+
+
+
 })
